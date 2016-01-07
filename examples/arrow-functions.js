90a75e219141a348adedb32235a7317df473362b
Juan Cancela <cancela.juancarlos@gmail.com>
2016-01-06 23:38:44 -0300

    added arrow function examples

--- examples/arrow-functions.js
+++ examples/arrow-functions.js
@@ -2,14 +2,14 @@
 // some key differences compared to legacy function definition methods in JS:
 // Theres no this, cant be instantiated with new, and no have no prototype.
 // Example:
-
 var binomio = (a, b) => (a * a) + (2 * a * b) + (b * b);
 
-//.. is the same than:
 
+//.. is the same than:
 function binomio(a, b){
   return (a * a) + (2 * a * b) + (b * b);
 }
+
 
 // Another example where arrow functions are optimal is for sorting. In example:
 var sortedPrices = arrayOfPrices.sort( (a, b) => a - b);
