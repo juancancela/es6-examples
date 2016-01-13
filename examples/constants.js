19375f89ec91b1f86a75d4fd60bce0810d3d214f
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2016-01-13 10:55:56 -0300

    Constants

---
+++ examples/constants.js
@@ -0,0 +1,6 @@
+// ES6, in opossition to what happen in ES5, make consts non writable, so they really act like a constant. 
+// So:
+
+const FLOOR = 56;
+
+// You wont be able to modify FLOOR const.
